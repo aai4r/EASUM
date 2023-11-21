5c8f49438e2713e8f718e49d850ed9a0408fe430
yewon95 <yewonhwang0816@gmail.com>
2023-11-21 18:05:32 +0900

    dataloader

---
+++ data_loader.py
@@ -0,0 +1,117 @@
+import os
+import cv2
+import glob
+import whisper
+import librosa
+from PIL import Image
+import numpy as np
+import torch
+import torchvision
+from torch.utils.data.dataset import Dataset
+from facenet_pytorch import MTCNN, InceptionResnetV1
+
+
+class create_data(Dataset):
+    def __init__(self, data_path):
+        super(create_data, self).__init__()
+        self.data_path = data_path
+        self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
+        self.mtcnn = MTCNN(image_size=160, margin=0)
+        self.video_list = []
+        for vid in ['006', '040']:
+            for f in glob.glob(os.path.join(self.data_path, vid, "movie/*.m2ts")):
+                self.video_list.append(f)
+        for dir in ['mp4', 'wav', 'jpg', 'cropped']:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+        with open(self.data_path + "sentiment_label.txt", "r") as f:
+            self.contents = f.readlines()
+
+    def extract_feat(self, file):
+        mts_f = file.split("/")[-1]
+        mp4_f = './mp4/' + mts_f.split('.')[0] + '.mp4'
+        wav_f = './wav/' + mts_f.split('.')[0] + '.wav'
+        self.mts2mp4(mp4_f, file)
+        self.video2wav(mp4_f, wav_f)
+        text = self.stt(wav_f)
+        img_feat = self.vid2img_embed(mp4_f)
+        audio_feat = self.audio_FE(wav_f)
+        return text, img_feat, audio_feat
+
+    def mts2mp4(self, mp4_file, mts_file):
+        threads = 4
+        os.system("ffmpeg -i %s -threads %d -f mp4 %s" % (mts_file, threads, mp4_file))
+        print("completed converting mts to mp4 for %s" % mts_file)
+        # if os.path.exists(new_filename):
+        #     os.remove(old_filename)
+
+    def video2wav(self, mp4_file, wav_file):
+        os.system("ffmpeg -i %s %s" % (mp4_file, wav_file))
+        print("completed converting mp4 to wav for %s" % mp4_file)
+        # if os.path.exists(new_filename):
+        #     os.remove(old_filename)
+
+    def stt(self, wav_file):
+        model = whisper.load_model("base")
+
+        # load audio and pad/trim it to fit 30 seconds
+        audio = whisper.load_audio(wav_file)
+        audio = whisper.pad_or_trim(audio)
+
+        # make log-Mel spectrogram and move to the same device as the model
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+        # detect the spoken language
+        _, probs = model.detect_language(mel)
+        print(f"Detected language: {max(probs, key=probs.get)}")
+
+        # decode the audio
+        options = whisper.DecodingOptions()
+        result = whisper.decode(model, mel, options)
+
+        return result.text
+
+    def vid2img_embed(self, path):
+        vidcap = cv2.VideoCapture(path)
+        success, image = vidcap.read()
+        count = 0
+        img_feat = []
+        while success:
+            success, image = vidcap.read()
+            print('Read a new frame: ', success)
+            if success is False:
+                break
+            vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 500))  # added this line
+            cv2.imwrite("./jpg/frame%d.jpg" % count, image)  # save frame as JPEG file
+            img = Image.open("./jpg/frame%d.jpg" % count)
+            img_cropped = self.mtcnn(img, save_path="./cropped/cropped_frame%d.jpg" % count)
+            img_embedding = self.resnet(img_cropped.unsqueeze(0)) # unsqueeze to add batch dimension
+            img_feat.extend(img_embedding.tolist())
+            count += 1
+        os.system("rm -r ./jpg/* ./cropped/* ./mp4/*")
+        return torch.tensor(img_feat)
+
+    def audio_FE(self, path):
+        y, sr = librosa.load(path)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=512, n_mfcc=64)
+        os.system("rm -r ./wav/*")
+        return torch.tensor(mfcc).permute(1, 0)  # shape: (n_mfcc, t)
+
+    def get_label(self, file):
+        num = int(file.split("-")[-1].split(".")[0])
+        label = float(self.contents[num-1].split(" ")[-1].strip())
+        return label
+
+    def __len__(self):
+        return len(self.video_list)
+
+    def __getitem__(self, idx):
+        selected_video = self.video_list[idx]
+        text, img_feat, audio_feat = self.extract_feat(selected_video)
+        label = self.get_label(selected_video)
+        return text, img_feat, audio_feat, label
+
+
+if __name__ == "__main__":
+    train_data = create_data(data_path="/home/yewon/ssd2/ai31/sentiment_analysis/Korean/audiotextvision-transformer/data/korean_multimodal_dataset/")
+    print(train_data[0])
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import os
+import cv2
 import whisper
+import torch
+from PIL import Image
+from facenet_pytorch import MTCNN, InceptionResnetV1
 
 
 def convert2mp4(path, mp4_file, mts_file):
@@ -39,7 +43,105 @@ def stt(wav_file):
     print(result.text)
 
 
+def vid2image(path):
+    vidcap = cv2.VideoCapture(path)
+    success, image = vidcap.read()
+    count = 0
+    while success:
+        success, image = vidcap.read()
+        print('Read a new frame: ', success)
+        if success is False:
+            break
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 500))  # added this line
+        cv2.imwrite("frame%d.jpg" % count, image)  # save frame as JPEG file
+        count += 1
+
+
+def video_FE(path):
+    mtcnn = MTCNN(image_size=160, margin=0)
+    resnet = InceptionResnetV1(pretrained='vggface2').eval()
+    img = Image.open(path)
+    img_cropped = mtcnn(img, save_path='./test.jpg')
+    img_embedding = resnet(img_cropped.unsqueeze(0))
+    print(img_embedding.shape)
+
+
+def crop(vid, i, j, h, w):
+    return vid[..., i:(i + h), j:(j + w)]
+
+
+def center_crop(vid, output_size):
+    h, w = vid.shape[-2:]
+    th, tw = output_size
+
+    i = int(round((h - th) / 2.))
+    j = int(round((w - tw) / 2.))
+    return crop(vid, i, j, th, tw)
+
+
+def normalize(vid, mean, std):
+    shape = (-1,) + (1,) * (vid.dim() - 1)
+    mean = torch.as_tensor(mean).reshape(shape)
+    std = torch.as_tensor(std).reshape(shape)
+    return (vid - mean) / std
+
+
+def to_normalized_float_tensor(vid):
+    return vid.permute(3, 0, 1, 2).to(torch.float32) / 255
+
+
+class CenterCrop(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, vid):
+        return center_crop(vid, self.size)
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, vid):
+        return normalize(vid, self.mean, self.std)
+
+
+class Resize(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, vid):
+        # NOTE: for those functions, which generally expect mini-batches, we keep them
+        # as non-minibatch so that they are applied as if they were 4d (thus image).
+        # this way, we only apply the transformation in the spatial domain
+        interpolation = 'bilinear'
+        # NOTE: using bilinear interpolation because we don't work on minibatches
+        # at this level
+        scale = None
+        if isinstance(self.size, int):
+            scale = float(self.size) / min(vid.shape[-2:])
+            size = None
+        else:
+            size = self.size
+        return torch.nn.functional.interpolate(
+            vid, size=size, scale_factor=scale, mode=interpolation, align_corners=False,
+            recompute_scale_factor=False
+        )
+
+
+class ToFloatTensorInZeroOne(object):
+    def __call__(self, vid):
+        return to_normalized_float_tensor(vid)
+
+
 if __name__ == "__main__":
+    # path="/home/yewon/ssd2/ai31/sentiment_analysis/Korean/audiotextvision-transformer/data/korean_multimodal_dataset/006/movie"
+    path = "/home/yewon/ssd2/ai31/sentiment_analysis/Korean/audiotextvision-transformer/data/korean_multimodal_dataset/000/picture/000-ang-00.JPG"
+    video_FE(path)
+    convert2mp4(path, './mp4/test.mp4', '006-004.m2ts')
+    vid2image(path+"/mp4/test.mp4")
+    video2wav(path, './mp4/test.mp4', './wav/test.wav')
     stt("./news_data/wav_files/SPK073/SPK073KBSCU058/SPK073KBSCU058M001.wav")
 
 
