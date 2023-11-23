2cd219079625d5087c1b2cb82d285f0dacbeeb5b
yewon95 <yewonhwang0816@gmail.com>
2023-11-23 16:50:16 +0900

    add test_loader

--- test_loader.py
+++ test_loader.py
@@ -13,46 +13,32 @@ from transformers import BertTokenizerFast, BertModel
 
 
 class create_dataset(Dataset):
-    def __init__(self, data_path, data_num):
+    def __init__(self, data_path):
         super(create_dataset, self).__init__()
         self.data_path = data_path
         self.max_len = 40
         self.tokenizer = BertTokenizerFast.from_pretrained("kykim/bert-kor-base")
         self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
-        self.mtcnn = MTCNN(image_size=160, margin=0)
+        self.mtcnn = MTCNN(image_size=160, margin=0, thresholds=[0.3, 0.3, 0.3])
         self.video_list = []
-        for vid in data_num:
-            for f in glob.glob(os.path.join(self.data_path, vid, "movie/*.m2ts")):
-                self.video_list.append(f)
-        for dir in ['mp4', 'wav', 'jpg', 'cropped']:
+        for f in glob.glob(os.path.join(self.data_path, "*")):
+            self.video_list.append(f)
+        for dir in ['wav', 'jpg', 'cropped']:
             if not os.path.exists(dir):
                 os.makedirs(dir)
-        with open(self.data_path + "sentiment_label.txt", "r") as f:
-            self.contents = f.readlines()
 
     def extract_feat(self, file):
-        mts_f = file.split("/")[-1]
-        mp4_f = './mp4/' + mts_f.split('.')[0] + '.mp4'
-        wav_f = './wav/' + mts_f.split('.')[0] + '.wav'
-        self.mts2mp4(mp4_f, file)
-        self.video2wav(mp4_f, wav_f)
+        img_feat = self.vid2img_embed(file)
+        file_name = file.split("/")[-1]
+        wav_f = './wav/' + file_name.split('.')[0] + '.wav'
+        self.video2wav(file, wav_f)
         text = self.stt(wav_f)
-        img_feat = self.vid2img_embed(mp4_f)
         audio_feat = self.audio_FE(wav_f)
         return text, img_feat, audio_feat
-
-    def mts2mp4(self, mp4_file, mts_file):
-        threads = 4
-        os.system("ffmpeg -i %s -threads %d -f mp4 %s" % (mts_file, threads, mp4_file))
-        print("completed converting mts to mp4 for %s" % mts_file)
-        # if os.path.exists(new_filename):
-        #     os.remove(old_filename)
 
     def video2wav(self, mp4_file, wav_file):
         os.system("ffmpeg -i %s %s" % (mp4_file, wav_file))
         print("completed converting mp4 to wav for %s" % mp4_file)
-        # if os.path.exists(new_filename):
-        #     os.remove(old_filename)
 
     def stt(self, wav_file):
         model = whisper.load_model("base")
@@ -96,19 +82,14 @@ class create_dataset(Dataset):
 
     def audio_FE(self, path):
         y, sr = librosa.load(path)
-        mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=512, n_mfcc=64)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=2048, n_mfcc=64)
         os.system("rm -r ./wav/*")
         return torch.tensor(mfcc).permute(1, 0)  # shape: (n_mfcc, t)
 
-    def get_label(self, file):
-        num = int(file.split("-")[-1].split(".")[0])
-        label = float(self.contents[num-1].split(" ")[-1].strip())
-        return label
-
     def prepare_bert_input(self, text):
-        input_ids = self.tokenizer.encode_plus(text[0])['input_ids']
-        segment_ids = self.tokenizer.encode_plus(text[0])['token_type_ids']
-        attention_mask = self.tokenizer.encode_plus(text[0])['attention_mask']
+        input_ids = self.tokenizer.encode_plus(text)['input_ids']
+        segment_ids = self.tokenizer.encode_plus(text)['token_type_ids']
+        attention_mask = self.tokenizer.encode_plus(text)['attention_mask']
         if len(input_ids) < self.max_len:
             input_ids.extend([0] * (self.max_len - len(input_ids)))
             segment_ids.extend([0] * (self.max_len - len(segment_ids)))
@@ -119,6 +100,16 @@ class create_dataset(Dataset):
             attention_mask = attention_mask[:self.max_len]
         return np.array(input_ids), np.array(attention_mask), np.array(segment_ids)
 
+    def collate(self, data):
+        if len(data) < self.max_len:
+            seq_len, dim = data.shape[0], data.shape[1]
+            target = torch.zeros(self.max_len, dim)
+            target[:seq_len, :] = data
+            return target
+        else:
+            data = data[:self.max_len]
+            return data
+
     def __len__(self):
         return len(self.video_list)
 
@@ -126,18 +117,17 @@ class create_dataset(Dataset):
         selected_video = self.video_list[idx]
         text, img_feat, audio_feat = self.extract_feat(selected_video)
         input_ids, attention_mask, segment_ids = self.prepare_bert_input(text)
-        label = self.get_label(selected_video)
+        video, audio = self.collate(img_feat), self.collate(audio_feat)
         input_dict = {
-            'audio': audio_feat,
-            'video': img_feat,
+            'audio': audio,
+            'video': video,
             'input_ids': torch.tensor(input_ids),
             'segment_ids': torch.tensor(segment_ids),
             'attention_mask': torch.tensor(attention_mask),
-            'label': torch.tensor([[label]])
         }
         return input_dict
 
 
 if __name__ == "__main__":
-    train_data = create_dataset(data_path="/home/yewon/ssd2/ai31/sentiment_analysis/Korean/audiotextvision-transformer/data/korean_multimodal_dataset/")
-    print(train_data[0])
+    train_data = create_dataset(data_path="./test_videos")
+    print(train_data[1])
